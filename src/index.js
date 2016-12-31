c830de779ee99e9ff7a5adb090739bca78ee2cf2
Alex Corvi <alex@arrayy.com>
2016-12-31 03:02:09 +0300

    wrapper around URL Valiation to encode urls and remove protocols

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ipChecker from "./tests/ip.js";
 import urlChecker from "./tests/url.js";
 import transform from "./transform/transform.js";
 import identify from "./transform/identify.js";
+import hasprotocol from "./tests/hasprotocol.js";
 
 const anchorme = function(str,options){
 	options = defaultOptions(options);
@@ -15,7 +16,14 @@ const anchorme = function(str,options){
 // exposing few functions for extra uses
 anchorme.validate = {
 	ip:ipChecker,
-	url:urlChecker,
+	url:function(input){
+		// simple wrapper that does what "identify.js" does initially
+		// remove the protocal
+		var protocol = hasprotocol(input) || "";
+		input = input.substr(protocol.length);
+		input = encodeURI(input);
+		return urlChecker(input);
+	},
 	email:emailChecker
 };
 
