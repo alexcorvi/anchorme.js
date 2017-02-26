18c4f5411cc382b85bf4bdbe94acc7f9da77789b
Alex Corvi <alex@arrayy.com>
2017-02-27 00:18:19 +0300

    use indexOf instead of startsWith

--- src/tests/hasprotocol.ts
+++ src/tests/hasprotocol.ts
@@ -1,10 +1,10 @@
 export default function (str:string):false|"http://"|"https://"|"ftp://"|"ftps://"|"file:///"|"mailto:" {
 	str = str.toLowerCase();
-	if(str.startsWith("http://")) return "http://";
-	else if(str.startsWith("https://")) return "https://";
-	else if(str.startsWith("ftp://")) return "ftp://";
-	else if(str.startsWith("ftps://")) return "ftps://";
-	else if(str.startsWith("file:///")) return "file:///";
-	else if(str.startsWith("mailto:")) return "mailto:";
+	if(str.indexOf("http://") === 0) return "http://";
+	else if(str.indexOf("https://") === 0) return "https://";
+	else if(str.indexOf("ftp://") === 0) return "ftp://";
+	else if(str.indexOf("ftps://") === 0) return "ftps://";
+	else if(str.indexOf("file:///") === 0) return "file:///";
+	else if(str.indexOf("mailto:") === 0) return "mailto:";
 	else return false;
 }
