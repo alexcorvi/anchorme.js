8aabc2d2440eec5390ebe097c8ff85a4588765ad
Alex Corvi <alex@arrayy.com>
2017-02-26 18:03:02 +0300

    removed old codebase (refractoring to typescript)

--- src/tests/hasprotocol.js
+++
@@ -1,9 +0,0 @@
-export default function (str) {
-	str = str.toLowerCase();
-	if(str.startsWith("http://")) return "http://";
-	else if(str.startsWith("https://")) return "https://";
-	else if(str.startsWith("ftp://")) return "ftp://";
-	else if(str.startsWith("file:///")) return "file:///";
-	else if(str.startsWith("mailto:")) return "mailto:";
-	else return false;
-}
