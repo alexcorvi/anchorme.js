0e0de0ec8dddb087a9b01c1899250e77259c03a3
alexcorvi <alex@arrayy.com>
2017-04-05 13:38:41 +0300

    1.0.5

--- src/transform/identify.ts
+++ src/transform/identify.ts
@@ -64,6 +64,7 @@ export default function (inputArr:Array<string>, options:Options) {
 		}
 
 		if(!urlObj) return fragment;
+		
 		else {
 			if((inputArr[index-1] === "'" || inputArr[index-1] === '"') && ~htmlAttrs.indexOf(inputArr[index-2]))
 				return fragment;
