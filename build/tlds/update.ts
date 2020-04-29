8c0caabeac6af941b3e8477f5ec25187a9759114
kmsheng <kmsh3ng@gmail.com>
2020-04-29 17:51:34 +0800

    check duplication when merging with local TLDs

--- build/tlds/update.ts
+++ build/tlds/update.ts
@@ -6,7 +6,12 @@ const localTLDs = ["TEST"];
 
 listRemoteTLDs().then(list => {
 
-	const listWithLocalTLDs = [...list, ...localTLDs];
+	const listWithLocalTLDs = localTLDs.reduce((list, localTLD) => {
+		if (! list.includes(localTLD)) {
+			return list.concat(localTLD)
+		}
+		return list
+	}, list)
 
 	fs.writeFileSync(
 		path.resolve("src/tlds.ts"),
