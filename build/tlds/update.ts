70b8610bd0c09e9c1d2761345f4bd274c50f3f41
kmsheng <kmsh3ng@gmail.com>
2020-04-29 17:40:14 +0800

    add const localTLDs to build/tlds/update.ts

--- build/tlds/update.ts
+++ build/tlds/update.ts
@@ -2,10 +2,15 @@ import { listRemoteTLDs } from "./list-remote";
 import * as fs from "fs";
 import * as path from "path";
 
+const localTLDs = ["TEST"];
+
 listRemoteTLDs().then(list => {
+
+	const listWithLocalTLDs = [...list, ...localTLDs];
+
 	fs.writeFileSync(
 		path.resolve("src/tlds.ts"),
-		`export const TLDs = \`(${list.join("|")})\`;`,
+		`export const TLDs = \`(${listWithLocalTLDs.join("|")})\`;`,
 		{
 			encoding: "UTF8"
 		}
